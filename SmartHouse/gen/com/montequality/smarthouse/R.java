7de8d0f85eb9292e2dc39d8616da124be0cd70fd
Bogdan Begovic <Bogdan Begovic@RS-DW-014.drey.rs>
2013-07-05 12:30:52 +0200

    Created Login

--- SmartHouse/gen/com/montequality/smarthouse/R.java
+++ SmartHouse/gen/com/montequality/smarthouse/R.java
@@ -15,6 +15,7 @@ public final class R {
     public static final class attr {
     }
     public static final class color {
+        public static final int blue=0x7f050001;
         public static final int white=0x7f050000;
     }
     public static final class dimen {
@@ -28,9 +29,15 @@ public final class R {
         public static final int activity_vertical_margin=0x7f060001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
-        public static final int smart_house_bcg=0x7f020001;
-        public static final int smart_house_bcg_pure=0x7f020002;
+        public static final int btn_default_disabled_focused_holo_light=0x7f020000;
+        public static final int btn_default_disabled_holo_light=0x7f020001;
+        public static final int btn_default_focused_holo_light=0x7f020002;
+        public static final int btn_default_pressed_holo_light=0x7f020003;
+        public static final int custom_blue_button=0x7f020004;
+        public static final int custom_progress=0x7f020005;
+        public static final int ic_launcher=0x7f020006;
+        public static final int smart_house_bcg=0x7f020007;
+        public static final int smart_house_bcg_pure=0x7f020008;
     }
     public static final class id {
         public static final int action_forgot_password=0x7f0a0007;
@@ -53,8 +60,8 @@ public final class R {
     }
     public static final class string {
         public static final int action_forgot_password=0x7f070008;
+        public static final int action_login_register=0x7f070006;
         public static final int action_settings=0x7f070001;
-        public static final int action_sign_in_register=0x7f070006;
         public static final int action_sign_in_short=0x7f070007;
         public static final int app_name=0x7f070000;
         public static final int copyright=0x7f07000e;
