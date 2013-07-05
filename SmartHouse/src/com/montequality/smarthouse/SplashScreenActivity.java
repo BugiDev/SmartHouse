982f562e31ed46fcbdea16a131d1f6b3d687e09d
Bogdan Begovic <Bogdan Begovic@RS-DW-014.drey.rs>
2013-07-05 11:29:24 +0200

    Created splash screen and new background

--- SmartHouse/src/com/montequality/smarthouse/SplashScreenActivity.java
+++ SmartHouse/src/com/montequality/smarthouse/SplashScreenActivity.java
@@ -1,15 +1,63 @@
 package com.montequality.smarthouse;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Bundle;
+import android.os.Handler;
 import android.view.Menu;
 
 public class SplashScreenActivity extends Activity {
+
+	private static final int SPLASH_TIME = 2 * 1000;// 3 seconds
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_splash_screen);
+		try {
+		new Handler().postDelayed(new Runnable() {
+
+			public void run() {
+				if(isNetworkAvailable()){
+					Intent intent = new Intent(SplashScreenActivity.this,
+							LoginActivity.class);
+						startActivity(intent);
+				}else {
+					
+				}
+				
+
+				SplashScreenActivity.this.finish();
+
+				overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+
+			}
+			
+			
+		}, SPLASH_TIME);
+		
+		new Handler().postDelayed(new Runnable() {
+			  public void run() {
+			         } 
+			    }, SPLASH_TIME);
+		} catch(Exception e){}
+	}
+
+	
+	@Override
+	public void onBackPressed() {
+		this.finish();
+		super.onBackPressed();
+	}
+	
+	private boolean isNetworkAvailable() {
+	    ConnectivityManager connectivityManager 
+	          = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+	    NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+	    return activeNetworkInfo != null && activeNetworkInfo.isConnected();
 	}
 
 	@Override
