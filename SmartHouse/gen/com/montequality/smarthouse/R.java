d860444907b6c7c93554d3218879b0efcec67dcc
bogdan_begovic <bogdan.begovic@benetech-corp.com>
2013-07-31 08:33:48 +0200

    Created TV remote.

--- SmartHouse/gen/com/montequality/smarthouse/R.java
+++ SmartHouse/gen/com/montequality/smarthouse/R.java
@@ -17,6 +17,7 @@ public final class R {
     public static final class color {
         public static final int blue=0x7f060001;
         public static final int grey=0x7f060002;
+        public static final int tranparent=0x7f060003;
         public static final int white=0x7f060000;
     }
     public static final class dimen {
@@ -43,51 +44,58 @@ public final class R {
         public static final int btn_default_focused_holo_light=0x7f02000a;
         public static final int btn_default_pressed_holo_light=0x7f02000b;
         public static final int camera=0x7f02000c;
-        public static final int custom_blue_button=0x7f02000d;
-        public static final int custom_dialog_border=0x7f02000e;
-        public static final int custom_progress=0x7f02000f;
-        public static final int devices=0x7f020010;
-        public static final int ic_launcher=0x7f020011;
-        public static final int led_off=0x7f020012;
-        public static final int led_on=0x7f020013;
-        public static final int lightbuble_left=0x7f020014;
-        public static final int lightbuble_right_off=0x7f020015;
-        public static final int lightbuble_right_on=0x7f020016;
-        public static final int list_more=0x7f020017;
-        public static final int premade_mods=0x7f020018;
-        public static final int proba_str=0x7f020019;
-        public static final int remote=0x7f02001a;
-        public static final int settings=0x7f02001b;
-        public static final int smart_house_bcg=0x7f02001c;
-        public static final int smart_house_bcg_pure=0x7f02001d;
-        public static final int sml=0x7f02001e;
-        public static final int temperature=0x7f02001f;
-        public static final int tv=0x7f020020;
-        public static final int warning=0x7f020021;
-        public static final int watch=0x7f020022;
+        public static final int ch_down=0x7f02000d;
+        public static final int ch_up=0x7f02000e;
+        public static final int custom_blue_button=0x7f02000f;
+        public static final int custom_dialog_border=0x7f020010;
+        public static final int custom_progress=0x7f020011;
+        public static final int custom_transparent_button=0x7f020012;
+        public static final int custom_transparent_button_full=0x7f020013;
+        public static final int devices=0x7f020014;
+        public static final int ic_launcher=0x7f020015;
+        public static final int led_off=0x7f020016;
+        public static final int led_on=0x7f020017;
+        public static final int lightbuble_left=0x7f020018;
+        public static final int lightbuble_right_off=0x7f020019;
+        public static final int lightbuble_right_on=0x7f02001a;
+        public static final int list_more=0x7f02001b;
+        public static final int premade_mods=0x7f02001c;
+        public static final int proba_str=0x7f02001d;
+        public static final int remote=0x7f02001e;
+        public static final int settings=0x7f02001f;
+        public static final int smart_house_bcg=0x7f020020;
+        public static final int smart_house_bcg_pure=0x7f020021;
+        public static final int sml=0x7f020022;
+        public static final int t_down=0x7f020023;
+        public static final int t_up=0x7f020024;
+        public static final int temperature=0x7f020025;
+        public static final int tv=0x7f020026;
+        public static final int v_down=0x7f020027;
+        public static final int v_up=0x7f020028;
+        public static final int warning=0x7f020029;
+        public static final int watch=0x7f02002a;
     }
     public static final class id {
-        public static final int action_forgot_password=0x7f0b0029;
-        public static final int action_settings=0x7f0b0028;
-        public static final int custom_toast_text=0x7f0b0027;
+        public static final int action_forgot_password=0x7f0b0030;
+        public static final int action_settings=0x7f0b002f;
+        public static final int custom_toast_text=0x7f0b002e;
         public static final int devices_button=0x7f0b000b;
-        public static final int dialog_action_text=0x7f0b001f;
-        public static final int dialog_button_cancle=0x7f0b001d;
-        public static final int dialog_button_ok=0x7f0b001c;
-        public static final int dialog_image=0x7f0b001a;
-        public static final int dialog_temperature_button_ok=0x7f0b0022;
-        public static final int dialog_temperature_image=0x7f0b0020;
-        public static final int dialog_temperature_text=0x7f0b0021;
-        public static final int dialog_text=0x7f0b001b;
+        public static final int dialog_action_text=0x7f0b0026;
+        public static final int dialog_button_cancle=0x7f0b0024;
+        public static final int dialog_button_ok=0x7f0b0023;
+        public static final int dialog_image=0x7f0b0021;
+        public static final int dialog_temperature_button_ok=0x7f0b0029;
+        public static final int dialog_temperature_image=0x7f0b0027;
+        public static final int dialog_temperature_text=0x7f0b0028;
+        public static final int dialog_text=0x7f0b0022;
         public static final int home_layout=0x7f0b0002;
         public static final int home_status=0x7f0b0000;
         public static final int home_status_message=0x7f0b0001;
-        public static final int imageButton1=0x7f0b0015;
         public static final int info_button=0x7f0b0006;
         public static final int info_text=0x7f0b000c;
-        public static final int list_icon_left=0x7f0b0023;
-        public static final int list_icon_right=0x7f0b0024;
-        public static final int list_text=0x7f0b0025;
+        public static final int list_icon_left=0x7f0b002a;
+        public static final int list_icon_right=0x7f0b002b;
+        public static final int list_text=0x7f0b002c;
         public static final int login=0x7f0b0012;
         public static final int login_form=0x7f0b000f;
         public static final int login_save_check=0x7f0b0014;
@@ -95,19 +103,27 @@ public final class R {
         public static final int login_status_message=0x7f0b000e;
         public static final int password=0x7f0b0011;
         public static final int premade_mods_button=0x7f0b000a;
-        public static final int progressBar1=0x7f0b001e;
+        public static final int progressBar1=0x7f0b0025;
         public static final int relativeLayout1=0x7f0b0003;
         public static final int settings_button=0x7f0b0007;
-        public static final int settings_button_logout=0x7f0b0018;
-        public static final int settings_sound_check=0x7f0b0016;
-        public static final int settings_vibra_check=0x7f0b0017;
+        public static final int settings_button_logout=0x7f0b001f;
+        public static final int settings_sound_check=0x7f0b001d;
+        public static final int settings_vibra_check=0x7f0b001e;
         public static final int sign_in_button=0x7f0b0013;
         public static final int textView1=0x7f0b0008;
         public static final int textView2=0x7f0b0004;
         public static final int textView3=0x7f0b0009;
         public static final int textView4=0x7f0b0005;
-        public static final int titleDividerTop=0x7f0b0019;
-        public static final int toast_layout=0x7f0b0026;
+        public static final int titleDividerTop=0x7f0b0020;
+        public static final int toast_layout=0x7f0b002d;
+        public static final int tv_remote_ch_down=0x7f0b0017;
+        public static final int tv_remote_ch_up=0x7f0b0018;
+        public static final int tv_remote_power=0x7f0b0015;
+        public static final int tv_remote_text_channel=0x7f0b001b;
+        public static final int tv_remote_text_room=0x7f0b001a;
+        public static final int tv_remote_text_volume=0x7f0b001c;
+        public static final int tv_remote_v_down=0x7f0b0016;
+        public static final int tv_remote_v_up=0x7f0b0019;
         public static final int username=0x7f0b0010;
     }
     public static final class layout {
@@ -156,22 +172,22 @@ public final class R {
         public static final int action_login_short=0x7f080024;
         public static final int action_settings=0x7f080001;
         public static final int app_name=0x7f080000;
-        public static final int copyright=0x7f080040;
+        public static final int copyright=0x7f080048;
         /**  Home menu 
          */
-        public static final int custom_list_image_left_description=0x7f08003e;
-        public static final int custom_list_image_right_description=0x7f08003f;
+        public static final int custom_list_image_left_description=0x7f080046;
+        public static final int custom_list_image_right_description=0x7f080047;
         /**  Home menu 
          */
         public static final int devices=0x7f080011;
         /**  Home menu descritpions
          */
         public static final int devices_desc=0x7f080015;
-        public static final int dialog_button_cancle=0x7f08003b;
-        public static final int dialog_button_ok=0x7f08003a;
-        public static final int dialog_description_network=0x7f080039;
-        public static final int dialog_text_network=0x7f08003d;
-        public static final int dialog_title_network=0x7f08003c;
+        public static final int dialog_button_cancle=0x7f080043;
+        public static final int dialog_button_ok=0x7f080042;
+        public static final int dialog_description_network=0x7f080041;
+        public static final int dialog_text_network=0x7f080045;
+        public static final int dialog_title_network=0x7f080044;
         public static final int error_field_required=0x7f08002a;
         public static final int error_incorrect_password=0x7f080029;
         public static final int error_invalid_password=0x7f080028;
@@ -197,24 +213,36 @@ public final class R {
         /**  Strings related to login 
          */
         public static final int prompt_username=0x7f080020;
+        public static final int remote_tv_ch_down_desc=0x7f080030;
+        /**  TV Remote descritpions
+         */
+        public static final int remote_tv_ch_up_desc=0x7f08002f;
+        public static final int remote_tv_channel=0x7f08002d;
+        public static final int remote_tv_power_desc=0x7f080033;
+        /**  TV Remote 
+         */
+        public static final int remote_tv_room=0x7f08002c;
+        public static final int remote_tv_v_down_desc=0x7f080032;
+        public static final int remote_tv_v_up_desc=0x7f080031;
+        public static final int remote_tv_volume=0x7f08002e;
         public static final int save_username_and_pass=0x7f080022;
         public static final int settings=0x7f080013;
-        public static final int settings_button_text=0x7f08002e;
+        public static final int settings_button_text=0x7f080036;
         public static final int settings_desc=0x7f080017;
-        public static final int settings_dialog_text=0x7f08002f;
-        public static final int settings_dialog_title=0x7f080030;
-        public static final int settings_logout_toast_message=0x7f080031;
-        public static final int settings_sound_disabled_toast_message=0x7f080033;
-        public static final int settings_sound_enabled_toast_message=0x7f080032;
-        public static final int settings_sound_text=0x7f08002c;
-        public static final int settings_vibra_disabled_toast_message=0x7f080035;
-        public static final int settings_vibra_enabled_toast_message=0x7f080034;
-        public static final int settings_vibra_text=0x7f08002d;
-        public static final int temperature_description=0x7f080043;
-        public static final int temperature_text=0x7f080042;
+        public static final int settings_dialog_text=0x7f080037;
+        public static final int settings_dialog_title=0x7f080038;
+        public static final int settings_logout_toast_message=0x7f080039;
+        public static final int settings_sound_disabled_toast_message=0x7f08003b;
+        public static final int settings_sound_enabled_toast_message=0x7f08003a;
+        public static final int settings_sound_text=0x7f080034;
+        public static final int settings_vibra_disabled_toast_message=0x7f08003d;
+        public static final int settings_vibra_enabled_toast_message=0x7f08003c;
+        public static final int settings_vibra_text=0x7f080035;
+        public static final int temperature_description=0x7f08004b;
+        public static final int temperature_text=0x7f08004a;
         /**  Home menu 
          */
-        public static final int temperature_title=0x7f080041;
+        public static final int temperature_title=0x7f080049;
         public static final int title_activity_aircondition_list=0x7f08000d;
         public static final int title_activity_boiler_list=0x7f08000a;
         public static final int title_activity_camera_list=0x7f08000e;
@@ -228,11 +256,11 @@ public final class R {
         public static final int title_activity_settings=0x7f080005;
         public static final int title_activity_tvlist=0x7f08000c;
         public static final int title_activity_window_blinds_list=0x7f08000f;
-        public static final int window_blinds_down=0x7f080037;
-        public static final int window_blinds_error=0x7f080038;
+        public static final int window_blinds_down=0x7f08003f;
+        public static final int window_blinds_error=0x7f080040;
         /**  Home menu 
          */
-        public static final int window_blinds_up=0x7f080036;
+        public static final int window_blinds_up=0x7f08003e;
     }
     public static final class style {
         /** 
